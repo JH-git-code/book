e36d4b5aeeaa38ce30d63646d1cce815759c7bfc
JackThompson_Hays <jack@brunosgoodsandgear.com>
2025-03-23 18:24:53 -0400

    Added to book repo

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='booklover',
+    version='1.0',
+    description='booklover files for M09 HW',
+    url='https://github.com/JH-git-code/book',
+    author='Jack Hays',
+    license='MIT',
+    packages=['booklover']
+
+)
